dc449bb3ddd6fc66a3e53f523153cf998256f064
julio <juljoseg@test.ca>
2018-05-22 15:16:26 -0400

    adding more tests

--- src/test/java/com/julio/controller/EmployeeControlerTest.java
+++ src/test/java/com/julio/controller/EmployeeControlerTest.java
@@ -9,6 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.julio.controller.EmployeeControler;
 import com.julio.entity.Employee;
 import com.julio.service.EmployeeService;
@@ -17,6 +18,7 @@ import com.julio.service.EmployeeService;
 import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 
 
@@ -33,7 +35,7 @@ public class EmployeeControlerTest {
 	
 	
 	@Test
-	public void testSearchEmployee() throws Exception
+	public void testSearchFoundEmployee() throws Exception
 	{
 		given(this.employeeService.findById(eq("1"))).willReturn(new Employee("10","Test","LastN"));
 		
@@ -46,16 +48,25 @@ public class EmployeeControlerTest {
 	
 	
 	
+	
 	@Test
 	public void testCreateEmployee() throws Exception
 	{
 		when(employeeService.save(any(Employee.class))).thenReturn(new Employee("11","Name11","LastName11"));
 		
 		
-		this.mvc.perform( post("/employees").content( "{ \"id\": \"10\",\"firstName\": \"Test\", \"lastName\": \"LastN\" } " ).contentType(MediaType.APPLICATION_JSON))
-			  .andExpect(status().isCreated())
-		      .andExpect( header().string("location","http://localhost/employees/11"));
+		this.mvc.perform( post("/employees").content( toJson(new Employee("11", "Name11", "LastName11")) )
+											.contentType(MediaType.APPLICATION_JSON))
+		  .andExpect(status().isOk())
+	      .andExpect( content().json("{ \"id\": \"11\",\"firstName\": \"Name11\", \"lastName\": \"LastName11\" } " ) );
 		
 	}
+	
+	
+	
+	private byte[] toJson(Object r) throws Exception {
+        ObjectMapper map = new ObjectMapper();
+        return map.writeValueAsString(r).getBytes();
+    }
 	
 }
